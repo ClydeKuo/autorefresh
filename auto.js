fe150021ae7e2fa111750c83970239d04d3d4254
guochijun@dianmi365.com <guochijun@dianmi365.com>
2017-04-01 11:51:44 +0800

    test

--- auto.js
+++ auto.js
@@ -1,19 +1,36 @@
-// Ìí¼ÓÒÀÀµ
-var request = require('request'); 
-var urls = ['http://blog.csdn.net/ClydeKuo/article/details/53741755','http://blog.csdn.net/ClydeKuo/article/details/54137922',"http://blog.csdn.net/ClydeKuo/article/details/54631321","http://blog.csdn.net/ClydeKuo/article/details/54928110","http://blog.csdn.net/ClydeKuo/article/details/54929081","http://blog.csdn.net/ClydeKuo/article/details/54966667"]; // ÌîÐ´ÄãÐèÒªË¢µÄÎÄÕÂµØÖ·
-var count = 0; // Ë¢ÁË¶àÉÙ´Î
-var len = urls.length; // ÐèÒªË¢µÄÎÄÕÂÆªÊý
-var co = 0; // ÎªÁËÑ­»·Ë¢ÐÂ
+// ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
+var request = require('request');
+// var urls = ['http://blog.csdn.net/ClydeKuo/article/details/53741755','http://blog.csdn.net/ClydeKuo/article/details/54137922',"http://blog.csdn.net/ClydeKuo/article/details/54631321","http://blog.csdn.net/ClydeKuo/article/details/54928110","http://blog.csdn.net/ClydeKuo/article/details/54929081","http://blog.csdn.net/ClydeKuo/article/details/54966667"]; // ï¿½ï¿½Ð´ï¿½ï¿½ï¿½ï¿½ÒªË¢ï¿½ï¿½ï¿½ï¿½ï¿½Âµï¿½Ö·
+var urls = ['http://adpays.net/v.php?user=4297']
+var count = 0; // Ë¢ï¿½Ë¶ï¿½ï¿½Ù´ï¿½
+var len = urls.length; // ï¿½ï¿½ÒªË¢ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Æªï¿½ï¿½
+var co = 0; // Îªï¿½ï¿½Ñ­ï¿½ï¿½Ë¢ï¿½ï¿½
 setInterval(function() {
     count = count + 1;
-    request(urls[co], function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log('Article '+co+' has refreshed '+Math.ceil(count/len)+' times');
-        // Ã¿ÆªÎÄÕÂË¢µÄ´ÎÊý=count/len
-      }
-        })
+    var heaLogin = {
+        'Host': 'www.otohits.net',
+        'Connection': 'close',
+        'Cache-Control': 'max-age=0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Origin': 'https://www.otohits.net',
+        'Upgrade-Insecure-Requests': 1,
+        // 'X-Requested-With':'XMLHttpRequest',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept-Encoding': 'gzip,deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.8',
+    }
+    request({
+        headers: heaLogin,
+        url:urls[co]
+    }, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log('Article ' + co + ' has refreshed ' + Math.ceil(count / len) + ' times');
+            // Ã¿Æªï¿½ï¿½ï¿½ï¿½Ë¢ï¿½Ä´ï¿½ï¿½ï¿½=count/len
+        }
+    })
     co = co + 1;
     if (co == len) {
-            co = 0;
+        co = 0;
     }
-}, 5000); // ÕâÀïµÄ5000  ´ú±íµÄ 5*1000msÖ´ÐÐÒ»´Î 
+}, 5); // ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½5000  ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ 5*1000msÖ´ï¿½ï¿½Ò»ï¿½ï¿½
