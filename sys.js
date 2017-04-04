1c1b3dd9694b5ebbf6b19d5d853a53d7c21f7ee7
ClydeKuo <clydekuo6@gmail.com>
2017-04-04 10:02:12 +0800

    dssadfsdf

--- sys.js
+++ sys.js
@@ -1,44 +1,50 @@
 var execFile = require('child_process').execFile;
 var spawn = require('child_process').spawn;
-var ps = spawn('ps', ['ef']);
+function checkauto(){
+    var ps = spawn('ps', ['-ef']);
+    var grep = spawn('grep', ['node']);
+    ps.stdout.on('data', function(data){
+      grep.stdin.write(data);
+    });
+    ps.on('close', function(code){
+      if (code !== 0) {
+        // console.log('ps 进程退出码：'+code);
+      }
+             grep.stdin.end();
+    });
+    grep.stdout.on('data', function(data){
+        var temp=data.toString().match(/autorefresh\/loginIndex/)
+      console.log(temp);
+      if(!temp){
+          auto()
+      }
+    });
+    grep.on('close', function(code){
+      if (code !== 0) {
+        // console.log('grep 进程退出码：'+code);
+      }
+    });
+}
 
-var child = execFile('node', ['--version'],function(error, stdout, stderr){
-  if (error) {
-    throw error;
-  }
-  console.log(stdout);
-});
+function auto(){
+    var nohup=spawn('nohup',['>/dev/null 2>&1','&'])
+    var nodetype=spawn('node',['/usr/workstation/autorefresh/loginIndex.js'])
+    nohup.stdout.on('data', function(data){
+      nodetype.stdin.write(data);
+    });
+    nohup.on('close', function(code){
+      if (code !== 0) {
+        // console.log('ps 进程退出码：'+code);
+      }
+             grep.stdin.end();
+    });
+    nodetype.stdout.on('data', function(data){
 
-var grep = spawn('grep', ['node']);
-ps.stdout.on('data', function(data){
-  grep.stdin.write(data);
-});
-
-// ps.stderr.on('data', function(data){
-//   console.log('ps stderr: '+data);
-// });
-// nohup node /usr/workstation/autorefresh/loginIndex.js >/dev/null 2>&1 &
-ps.on('close', function(code){
-  if (code !== 0) {
-    // console.log('ps 进程退出码：'+code);
-  }
-  grep.stdin.end();
-});
-
-grep.stdout.on('data', function(data){
-    var temp=data.toString().match(/autorefresh\/loginIndex/)
-  console.log(temp);
-  if(!temp){
-
-  }
-});
-
-// grep.stderr.on('data', function(data){
-//   console.log('grep stderr: '+data);
-// });
-
-grep.on('close', function(code){
-  if (code !== 0) {
-    // console.log('grep 进程退出码：'+code);
-  }
-});
+    });
+    nodetype.on('close', function(code){
+      if (code !== 0) {
+        // console.log('grep 进程退出码：'+code);
+      }
+    });
+}
+setInterval(checkauto,1000)
